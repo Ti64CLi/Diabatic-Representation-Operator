3088bc67c1a8221adece9f35093929daed80f39f
TI64CLi <neuronix9302@gmail.com>
2025-05-21 17:06:39 +0200

    Separate symmetry stuffs from the rest

---
+++ symmetry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 21 09:54:05 2025
+
+@author: elidumont
+"""
+
+class Symmetry:
+    A1 = 0
+    A2 = 1
+    B1 = 2
+    B2 = 3
+    E = 4
