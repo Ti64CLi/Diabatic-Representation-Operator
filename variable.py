78acac3467b6d35694f8c14ef81762834a98b7ab
Elie Dumont <elie.dumont@ens-rennes.fr>
2025-05-26 11:00:25 +0200

    Implement variable as dataclass

---
+++ variable.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from symmetry import Symmetry
+from utils import *
+
+@dataclass(frozen=True)
+class Variable:
+    name: str
+    symmetry: Symmetry
+    complex_conjugate: bool = False
+
+    def weight(self) -> int:
+        sign = -1 if self.complex_conjugate else 1
+
+        return sign * self.symmetry.weight()
+    
+    def __str__(self) -> str:
+        s = f"{self.name}" + sign2sub(-1 if self.complex_conjugate else 1)
+
+        return s + (num2sub(self.symmetry.gamma) if self.symmetry.is_E() and self.symmetry.gamma > 1 else "")
+    
+    def __eq__(self, other) -> bool:
+        assert isinstance(other, Variable)
+
+        return self.name == other.name and self.symmetry == other.symmetry # is complex_conjugate equal or not ?
