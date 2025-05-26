8bc5cd0ecf8d92d3d395ae2362da6159b41e6990
Elie Dumont <elie.dumont@ens-rennes.fr>
2025-05-26 16:08:34 +0200

    Implement conjugation of variables

--- variable.py
+++ variable.py
@@ -8,17 +8,20 @@ class Variable:
     symmetry: Symmetry
     complex_conjugate: bool = False
 
-    def weight(self) -> int:
-        sign = -1 if self.complex_conjugate else 1
-
-        return sign * self.symmetry.weight()
-    
     def __str__(self) -> str:
         s = f"{self.name}{sign2sub(-1 if self.complex_conjugate else 1)}"
 
         return s + (num2sub(self.symmetry.gamma) if self.symmetry.is_E() and self.symmetry.gamma > 1 else "")
-    
+
     def __eq__(self, other) -> bool:
         assert isinstance(other, Variable)
 
         return self.name == other.name and self.symmetry == other.symmetry # is complex_conjugate equal or not ?
+
+    def conjugate(self) -> Variable:
+        return Variable(self.name, self.symmetry, complex_conjugate=not self.complex_conjugate)
+
+    def weight(self) -> int:
+        sign = -1 if self.complex_conjugate else 1
+
+        return sign * self.symmetry.weight()
