2e85d65db2883d38406057f1de4cc6fdb45e5cd1
Elie Dumont <elie.dumont@ens-rennes.fr>
2025-05-26 10:32:31 +0200

    Implement symmetry as data class

--- symmetry.py
+++ symmetry.py
@@ -6,9 +6,20 @@ Created on Wed May 21 09:54:05 2025
 @author: elidumont
 """
 
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass(frozen=True)
 class Symmetry:
-    A1 = 0
-    A2 = 1
-    B1 = 2
-    B2 = 3
-    E = 4
+    label: str                  # can be A1, A2, B1, B2 or E
+    gamma: Optional[int] = None # only for E_gamma
+
+    def is_E(self) -> bool:
+        return self.label == "E" and self.gamma is not None
+    
+    def weight(self) -> int:
+        return self.gamma if self.is_E() else 0
+    
+    def __str__(self):
+        return f"E_{self.gamma}" if self.is_E() else self.label
+
