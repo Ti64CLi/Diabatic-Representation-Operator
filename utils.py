d3ebd6d992bdbbbdfa7f9b3b612bc830e3594655
Elie Dumont <elie.dumont@ens-rennes.fr>
2025-05-26 10:49:50 +0200

    Add a separate file for utilities

---
+++ utils.py
@@ -0,0 +1,40 @@
+#################
+### utilities ###
+#################
+
+sign_sup = "⁻⁰⁺"
+sign_sub = "₋₀₊"
+num_sup = "⁰¹²³⁴⁵⁶⁷⁸⁹"
+num_sub = "₀₁₂₃₄₅₆₇₈₉"
+
+def num2sub(n: int) -> str:
+    if n == 0:
+        return num_sub[0]
+
+    n = abs(n)
+    s = ""
+
+    while n > 0:
+        s = num_sub[n % 10] + s
+        n //= 10
+
+    return s
+
+def num2sup(n: int) -> str:
+    if n == 0:
+        return num_sup[0]
+
+    n = abs(n)
+    s = ""
+
+    while n > 0:
+        s = num_sup[n % 10] + s
+        n //= 10
+
+    return s
+
+def sign2sub(n: int) -> str:
+    return sign_sub[1 + n]
+
+def sign2sup(n: int) -> str:
+    return sign_sup[1 + n]
