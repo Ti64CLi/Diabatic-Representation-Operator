9b9cb743c7abfab57390ffb3257a1e0e62cb917a
Elie Dumont <elie.dumont@ens-rennes.fr>
2025-05-26 16:10:07 +0200

    Implement monomials and invariants generation

--- monome.py
+++ monome.py
@@ -10,16 +10,33 @@ class Monome:
     variables: list[Variable]
 
     def weight(self) -> int:
-        return sum(v.weight() for v in variables)
-    
+        return sum(v.weight() for v in self.variables)
+
     def weight_mod(self, n: int) -> int:
         return self.weight() % n
-    
+
     def is_factorisable(self, n: int) -> bool:
         return len(self.variables) > 0 and self.weight_mod(n) == 0
-    
+
+    def is_real(self):
+        if len(self.variables) == 0:
+            return True
+
+        cv = Counter(self.variables)
+
+        for v in cv:
+            if cv[v] != cv[v.conjugate()]:
+                return False
+
+        return True
+
     def __str__(self) -> str:
-        return ''.join(str(v) for v in self.variables)
+        cv = Counter(self.variables)
+
+        if len(+cv) == 0:
+            return "1"
+
+        return ''.join((f"{v}{num2sup(cv[v])}" if cv[v] > 1 else f"{v}") for v in cv)
 
 def generate_monoms(variables: list[Variable], order: int, n: int, remove_factorizable: bool = True) -> list[Monome]:
     """
@@ -33,10 +50,10 @@ def generate_monoms(variables: list[Variable], order: int, n: int, remove_factor
         if remove_factorizable and m.is_factorisable(n):
             # add treatement for invariant factors ?
             continue
-        
+
         monoms.append(m)
-    
-    return m
+
+    return monoms
 
 def generate_variables_list(nvarsym: list[int], n: int) -> list[Variable]:
     """
@@ -44,18 +61,18 @@ def generate_variables_list(nvarsym: list[int], n: int) -> list[Variable]:
     """
     if len(nvarsym) == 0:
         return []
-    
+
     variables = []
 
     # generate A1 variables
     for i in range(nvarsym[0]):
         variables.append(Variable(f"R{i + 1}", Symmetry("A1")))
-    
+
     # generate A2 variables
     if len(nvarsym) > 1:
         for i in range(nvarsym[1]):
             variables.append(Variable(f"ρ{i + 1}", Symmetry("A2")))
-    
+
     # generate B1 variables
     if len(nvarsym) > 2:
         if nvarsym[2] > 0:
@@ -63,7 +80,7 @@ def generate_variables_list(nvarsym: list[int], n: int) -> list[Variable]:
 
         for i in range(nvarsym[2]):
             variables.append(Variable(f"Rb{i + 1}", Symmetry("B1", gamma=n//2)))
-    
+
     # generate B2 variables
     if len(nvarsym) > 3:
         if nvarsym[3] > 0:
@@ -71,7 +88,7 @@ def generate_variables_list(nvarsym: list[int], n: int) -> list[Variable]:
 
         for i in range(nvarsym[3]):
             variables.append(Variable(f"ρb{i + 1}", Symmetry("B2", gamma=n//2)))
-    
+
     # generate E variables
     if len(nvarsym) > 4:
         for gamma3 in range(4, len(nvarsym)):
@@ -80,5 +97,55 @@ def generate_variables_list(nvarsym: list[int], n: int) -> list[Variable]:
                 sym = Symmetry("E", gamma=gamma3 - 3)
                 variables.append(Variable(name, sym))
                 variables.append(Variable(name, sym, complex_conjugate=True)) # add Q+ and Q-
-    
+
     return variables
+
+def try_to_factorize(monome: Monome, factors: list[Monome]) -> bool:
+    target = Counter(monome.variables)
+    degree = target.total()
+
+    for factor in factors:
+        combo = Counter(factor.variables)
+        combo_degree = combo.total()
+
+        if degree < combo_degree:
+            continue
+
+        if combo <= target:
+            return True
+
+    return False
+
+def find_fundamental_invariants(variables: list[Variable], max_order: int, n: int) -> list[Monome]:
+    """
+    Find the fundamental invariants, those which cannot be factorize anymore
+    """
+    fundamentals = []
+
+    for order in range(1, max_order + 1):
+        monoms = generate_monoms(variables, order, n, remove_factorizable=False)
+
+        for m in monoms:
+            if m.weight_mod(n) != 0:
+                continue
+
+            if not try_to_factorize(m, fundamentals):
+                fundamentals.append(m)
+
+    return [Monome(variables=())] + fundamentals
+
+def generate_appearing_monoms(variables: list[Variable], n: int) -> list[Monome]:
+    """
+    Generate all appearing monomials
+    """
+    monoms = []
+
+    for order in range(n):
+        monoms.extend(generate_monoms(variables, order, n, True))
+
+    return monoms
+
+def generate_rhos(variables: list[Variable], n: int) -> list[Monome]:
+    invariants = find_fundamental_invariants(variables, n, n)
+
+    return [Monome(variables=())] + [inv for inv in invariants if not inv.is_real()]
